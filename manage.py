2f0379ce39ab037032d1c11fec278c27b29f7633
Thomas Sørensen <tsorensen007@gmail.com>
2024-09-03 17:19:49 +0200

    feat: legg til manage.py

---
+++ manage.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect('mydatabase.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT 0
+    )
+''')
